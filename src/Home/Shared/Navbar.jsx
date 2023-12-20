f5efe20f0594acbedefa7ddc5e085c83a31ab0e4
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-12-20 23:08:03 +0600

    add

--- src/Home/Shared/Navbar.jsx
+++ src/Home/Shared/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div>
@@ -25,21 +27,13 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <a>Item 1</a>
+                <Link>About</Link>
               </li>
               <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
+                <Link>About</Link>
               </li>
               <li>
-                <a>Item 3</a>
+                <Link>About</Link>
               </li>
             </ul>
           </div>
@@ -48,28 +42,18 @@ const Navbar = () => {
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
             <li>
-              <a>Item 1</a>
+              <Link>About</Link>
             </li>
             <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
+              <Link>About</Link>
             </li>
             <li>
-              <a>Item 3</a>
+              <Link>About</Link>
             </li>
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <Link to="login">Login</Link>
         </div>
       </div>
     </div>
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Home/Home";
 import Main from "../layout/Main";
+import Login from "../Login";
+import Register from "../Register";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+      {
+        path: "register",
+        element: <Register></Register>,
       },
     ],
   },
